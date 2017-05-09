2a0bfa1cb67f25c29c0d77e8db89e4ab28c05da0
Michael Weiss <quigleyforever@gmail.com>
2017-05-09 06:04:18 +0000

    first commit

---
+++ repeat.c
@@ -0,0 +1,20 @@
+//Michael Weiss]
+//IT 373
+//Cloud.9
+
+#include <stdio.h>
+
+int main()
+{
+    int num=123;
+    int sum=0;
+    
+    sum=sum+num;
+    sum=sum+num;
+    sum=sum+num;
+    sum=sum+num;
+    
+    printf("%d\n\r",sum);
+    
+    return 0;
+}
